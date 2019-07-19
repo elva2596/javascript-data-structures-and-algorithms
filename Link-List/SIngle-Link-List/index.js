90d3111cbefd934830bc72cd5bd46e349c7e032c
dailu.dl <dailu.dl@alibaba-inc.com>
2019-07-19 15:59:18 +0800

    feat:add append print getNode into linkList

--- Link-List/SIngle-Link-List/index.js
+++ Link-List/SIngle-Link-List/index.js
@@ -6,5 +6,43 @@ class Node{
 }
 
 class LinkList{
-  
+  constructor(value) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(node) {
+    if(!this.length){
+      this.head = node;
+      this.tail = node;
+
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+
+  print() {
+    const ret = [];
+    let currentNode = this.head;
+
+    while(currentNode){
+      ret.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    
+    return ret.join('->');
+  }
+
+  getNode(index){
+    let currentNode = this.head, curIndex = 0;
+
+    while(curIndex < index) {
+      curIndex++;
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
 }
