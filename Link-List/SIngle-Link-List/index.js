8745a58b11c72beadb10539fa2d7c4756dd6a156
dailu.dl <dailu.dl@alibaba-inc.com>
2019-07-23 10:54:23 +0800

    feat: add append insert removeAt remove findIndex function into linkedList

--- Link-List/SIngle-Link-List/index.js
+++ Link-List/SIngle-Link-List/index.js
@@ -6,43 +6,107 @@ class Node{
 }
 
 class LinkList{
-  constructor(value) {
+  constructor() {
     this.head = null;
-    this.tail = null;
     this.length = 0;
   }
 
-  append(node) {
-    if(!this.length){
+  append(element) {
+    const node = new Node(element);
+    let current = this.head;
+
+    if(!this.head) {
       this.head = node;
-      this.tail = node;
 
     } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
-  }
 
-  print() {
-    const ret = [];
-    let currentNode = this.head;
+      while(current) {
+        current = current.next;
+      }
 
-    while(currentNode){
-      ret.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-    
-    return ret.join('->');
-  }
-
-  getNode(index){
-    let currentNode = this.head, curIndex = 0;
-
-    while(curIndex < index) {
-      curIndex++;
-      currentNode = currentNode.next;
+      current.next = node;
     }
 
-    return currentNode;
+    this.length++;
   }
+
+  /**
+   * @description: 添加指定位置元素
+   * @param {type} 
+   * @return: 
+   */
+  insert(element, position) {
+    if(position < 0 || position > this.length){
+      console.error(`the position you insert is invalid`)
+       return false;
+    }
+
+    const node = new Node(element);
+    let current = this.head, previous, index = 0;
+
+    if(position === 0) {
+      node.next = current;
+      this.head = node;
+
+    } else {
+
+      while(index < position) {
+        previous = current;
+        current = current.next;
+        index++;
+      }
+
+      nodex.next = current;
+      previous.next = node;
+    }
+
+    this.length++;
+    return true;
+  }
+
+  removeAt(position) {
+    if(position < 0 || position >= this.length) {
+      console.error(`the position you remove is invalid`)
+      return null;
+    }
+
+    let current = this.head, current, index = 0;
+
+    if(position === 0) {
+      this.head = current.next;
+    } else {
+
+      while(index < position) {
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+    this.length--;
+    return current.value;
+  }
+
+  findIndex(element) {
+    let current = this.head, index = 0;
+
+    while(current) {
+      if(current.value === element) { return index; }
+      index++;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  /**
+   * @description: 删除指定文档
+   * @param {Any} 
+   * @return {Any}
+   */
+  remove(element) {
+    const index = this.findIndex(element);
+    return this.removeAt(index)
+  }
+
+  
 }
